74671e99d795709f82c091568076d43196e434b2
Brett Hevia <brett.hevia@gmail.com>
2016-12-09 20:35:22 -0700

    add jquery and babel

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const $ = require('jquery')
 
+const h1 = $('#testing')
+
+h1.on('click', () => {
+  console.log('hooray')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   entry: "./index.js",
-      output: {
-          path: __dirname,
-          filename: "bundle.js"
-      },
+  output: {
+      path: __dirname,
+      filename: "bundle.js"
+  },
+  module: {
+      loaders: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      ]
+    }
 };
